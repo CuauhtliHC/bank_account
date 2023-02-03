aa0c74a27a9e366a9882d848c309db5ddbe6f519
Cuauhtli <cuau_daali@hotmail.com>
2023-02-02 23:59:36 -0600

    Cleaning code

--- account.py
+++ account.py
@@ -1,11 +1,14 @@
+"""Modulo para importar clear y cls"""
 from os import system, name
 
 class Persona:
+    """Clase para generar personas"""
     def __init__(self, nombre, apellido):
         self.nombre = nombre
         self.apellido = apellido
 
 class Cliente(Persona):
+    """Clase para generar clientes"""
     def __init__(self, nombre, apellido, numero_cuenta, balance = 0):
         super().__init__(nombre, apellido)
         self.numero_cuenta = numero_cuenta
@@ -17,47 +20,52 @@ Número de Cuenta: {self.numero_cuenta}
 Balance: ${self.balance}"""
 
     def depositar(self, cantidad):
+        """Funcion para depositar"""
         self.balance += cantidad
 
     def retirar(self, cantidad):
+        """Funcion para retirar"""
         if cantidad > self.balance:
             print("Saldo insuficiente")
         else:
             self.balance -= cantidad
 
 def clear():
+    """Funcion para limpiar la consola"""
     if name == 'nt':
         system('cls')
     else:
         system('clear')
 
-def crearCliente():
+def crear_cliente():
+    """Funcion para crear cliente"""
     clear()
-    nombreCl = input("Ingrese su nombre: ")
-    apellidoCl = input("Ingrese su apellido: ")
-    accountNum = input("Ingrese su numero de cuenta: ")
-    cliente = Cliente(nombreCl, apellidoCl, accountNum)
+    nombre_cliente = input("Ingrese su nombre: ")
+    apellido_cliente = input("Ingrese su apellido: ")
+    account_num = input("Ingrese su numero de cuenta: ")
+    cliente = Cliente(nombre_cliente, apellido_cliente, account_num)
     return cliente
 
 
 
-def inicio():   
-    miCliente = crearCliente()
+def inicio():
+    """Funcion que inicia el programa"""
+    mi_cliente = crear_cliente()
     opcion = None
     while opcion != "3":
         clear()
-        print(miCliente)
-        print("""[1] Depositar 
+        print(mi_cliente)
+        print("""[1] Depositar
 [2] Retirar
 [3] Salir""")
         opcion = input("Elija una opción: ")
         match opcion:
             case "1":
                 cantidad = float(input("Ingrese la cantidad a depositar: $"))
-                miCliente.depositar(cantidad)
+                mi_cliente.depositar(cantidad)
             case "2":
                 cantidad = float(input("Ingrese la cantidad a retirar: $"))
-                miCliente.retirar(cantidad)
+                mi_cliente.retirar(cantidad)
             case "3":
                 print("Gracias por utilizar el sistema")
             case _:
